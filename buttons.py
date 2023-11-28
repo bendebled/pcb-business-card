c18fc1405ef4d880386c543f7ad2de8e529cc1f1
Benoit Debled <bede@hms-networks.com>
2023-11-28 18:45:12 +0100

    micropython app: fix buttons
    temporary buttons were used and not correctly wired during
    prototyping phase. This commit assigns the button's pins correctly

--- buttons.py
+++ buttons.py
@@ -1,9 +1,9 @@
 from machine import Pin
 
-LEFT_BTN_GPIO = 0
-RIGHT_BTN_GPIO = 1
+LEFT_BTN_GPIO = 1
+RIGHT_BTN_GPIO = 2
 UP_BTN_GPIO = 3
-DOWN_BTN_GPIO = 4
+DOWN_BTN_GPIO = 0
 
 class Buttons:
 
@@ -15,9 +15,9 @@ class Buttons:
 
     def manage_up_down_values(self, value, min_value, max_value):
         if self.down_btn.value() == 0:
-            value -= 1
-        if self.up_btn.value() == 0:
             value += 1
+        if self.up_btn.value() == 0:
+            value -= 1
         return max(min_value, min(max_value, value))
     
     def is_right_pressed(self):
