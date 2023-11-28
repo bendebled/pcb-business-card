45151badfe9893797b74f3d1648e0ee5426ae60c
Benoit Debled <bede@hms-networks.com>
2023-11-28 19:35:18 +0100

    upload script

---
+++ upload_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ampy -p /dev/ttyACM0 put app.py
+ampy -p /dev/ttyACM0 put app_secrets.py
+ampy -p /dev/ttyACM0 put buttons.py
+ampy -p /dev/ttyACM0 put colorsys.py
+ampy -p /dev/ttyACM0 put config.py
+ampy -p /dev/ttyACM0 put display.py
+ampy -p /dev/ttyACM0 put ds1621.py
+ampy -p /dev/ttyACM0 put led_effect.py
+ampy -p /dev/ttyACM0 put menu.py
+ampy -p /dev/ttyACM0 put microdot.mpy
+ampy -p /dev/ttyACM0 put microdot_asyncio.mpy
+ampy -p /dev/ttyACM0 put music.py
+ampy -p /dev/ttyACM0 put numberfacts.py
+ampy -p /dev/ttyACM0 put pong.py
+ampy -p /dev/ttyACM0 put power_mgmt.py
+ampy -p /dev/ttyACM0 put resume.py
+ampy -p /dev/ttyACM0 put settings.py
+ampy -p /dev/ttyACM0 put sh1106.py
+ampy -p /dev/ttyACM0 put statemachine.py
+ampy -p /dev/ttyACM0 put tetris.py
+ampy -p /dev/ttyACM0 put webserver.py
+ampy -p /dev/ttyACM0 put temperature.py
+ampy -p /dev/ttyACM0 put index.html
+ampy -p /dev/ttyACM0 put static
