9335c78d02b433601aa376a12ef17e9a4b4a2aca
Benoit Debled <bede@hms-networks.com>
2023-12-10 20:19:23 +0100

    micropython app: temperature fix

--- temperature.py
+++ temperature.py
@@ -7,9 +7,10 @@ class Temperature:
         self.temp = 0
         self.last_read = -999
         self.sensor = ds1621.DS1621(i2c=i2c)
+        self.read_temp(force=True)
 
-    def read_temp(self):
-        if(time.time() > self.last_read + 60):
+    def read_temp(self, force=False):
+        if force or (time.time() > self.last_read + 60):
             self.sensor.start_conversion()
             self.temp = self.sensor.read_temperature()/2
         return self.temp
